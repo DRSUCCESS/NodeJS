985370366fae25f55f002550193256b2e81fe9db
drsuccess <a4surprise@gmail.com>
2020-12-08 13:01:04 +0100

    Async read & write

--- read_write.js
+++ read_write.js
@@ -1,7 +1,7 @@
-var fs = require('fs')
+var fs = require('fs');
 
-//Sync means read this file before running below codes
-var readme = fs.readFileSync('readme.txt', 'utf8'); // read file 
-fs.writeFileSync('writeme.txt', readme) // create & write to another file
-
-// code
+fs.readFile('readme.txt', 'utf8', function (err, data) {
+    fs.writeFile('writeme.txt', data, function (err, result) {
+        if (err) console.log('error', err);
+    });
+});
---
+++ writeme.txt
@@ -0,0 +1,1 @@
+Yay, see me here
