f1910a7fa924fbe6ce0c8fb9e60f3a34ef504054
drsuccess <a4surprise@gmail.com>
2020-12-09 12:37:50 +0100

    use  buffer to store chunks

---
+++ readable_stream.js
@@ -0,0 +1,20 @@
+var http =  require('http');
+var fs = require('fs');
+
+var myReadStream =  fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
+});
+
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made:' +req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hey Ninjas');
+// });
+
+
+// server.listen(3000, '127.0.0.1');
+// console.log('Yo dqgs, now listening to port 3000');
