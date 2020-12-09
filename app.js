2cd5b77216cb58452dee7bb5bb30d4210cee5ce2
drsuccess <a4surprise@gmail.com>
2020-12-09 13:27:50 +0100

    serving HTML page

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made:' +req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     
-    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); //read file
-    var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8'); //read file
     myReadStream.pipe(res);//get data from readable string to the client (res obj)
 
 });
