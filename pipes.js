51afa2a5bea5863762478dd204caa137f717a18a
drsuccess <a4surprise@gmail.com>
2020-12-09 13:06:17 +0100

    pipe to client

--- pipes.js
+++ pipes.js
@@ -1,20 +1,16 @@
 var http =  require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); //read file
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
-
-//shortest way for chunk; get data from readable string to writeable string
-myReadStream.pipe(myWriteStream);
-
-/*
 var server = http.createServer(function(req, res){
     console.log('request was made:' +req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey Ninjas');
+    
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); //read file
+    var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+    myReadStream.pipe(res);//get data from readable string to the client (res obj)
+
 });
 
 
 server.listen(3000, '127.0.0.1');
 console.log('Yo dqgs, now listening to port 3000');
-*/
