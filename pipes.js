937459ef7eb49b38ef82ce489c0f246330cea06f
drsuccess <a4surprise@gmail.com>
2020-12-09 12:59:23 +0100

    best way for chunk

--- pipes.js
+++ pipes.js
@@ -7,7 +7,7 @@ var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
 //shortest way for chunk; get data from readable string to writeable string
 myReadStream.pipe(myWriteStream);
 
-
+/*
 var server = http.createServer(function(req, res){
     console.log('request was made:' +req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -17,3 +17,4 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('Yo dqgs, now listening to port 3000');
+*/
