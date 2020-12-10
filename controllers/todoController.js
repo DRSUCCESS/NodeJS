28e1be3e65b31ff0714d90c6a764caac98cb4905
drsuccess <a4surprise@gmail.com>
2020-12-10 16:02:17 +0100

    Add & Delete dummy data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,22 @@
+var bodyParser = require('body-parser');
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function(app){
     app.get('/todo', function (req, res) {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
     
-    app.post('/todo', function (req, res) {
-
+    app.post('/todo', urlencodedParser, function(req, res) {
+        data.push(req.body); // use push to add data
+        res.json(data); //return the data to client-side
     });
 
-    app.delete('./todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        // filter(delete) data if formatted string has a space replacement of '-'
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 };
