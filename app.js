910bc15d73b2c741c8ea16aeb4e908566d3cbfa7
drsuccess <a4surprise@gmail.com>
2020-12-10 01:07:18 +0100

    Middleware & Static Files

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('stuff')); //static files
+
+/*
+// Middleware: is any code between res& req
+app.use('/assets', function(req, res, next){
+    console.log(req.url);
+    next(); // to move on to the next middleware
+}); //fire assets anywhere code is access
+*/
+
+
 
 // use render for 'views', sendFile for 'html' 
 // sendFile for 'html' needs full dir e.g res.sendFile(__dirname, PATH)
 // render for 'views' needs only path_name e.g. res.render('index')
+
+
 app.get('/', function (req, res) {
     res.render('index');
 });
