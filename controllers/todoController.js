344023dd8847942a6680e40795a3bc0e6fc65ff7
drsuccess <a4surprise@gmail.com>
2020-12-10 17:51:34 +0100

    Connect to MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,21 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb+srv://servenig_db:serve1212@cluster0.k0m5s.mongodb.net/todo?retryWrites=true&w=majority', { useUnifiedTopology: true }, { useNewUrlParser: true })
+
+
+// create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String //create a string schema
+});
+
+var Todo = mongoose.model('Todo', todoSchema); //create a new model and pass it to schema
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved'); //save to mongodb
+}) // create new Todo schema
+
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
